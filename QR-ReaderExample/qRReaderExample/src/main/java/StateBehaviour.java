414c1d487643bc14b6244d15eac9aec80fd476ed
cgarciae <cgarcia.e88@gmail.com>
2015-06-09 11:21:30 -0500

    finish state machine

--- QR-ReaderExample/qRReaderExample/src/main/java/StateBehaviour.java
+++ QR-ReaderExample/qRReaderExample/src/main/java/StateBehaviour.java
@@ -4,54 +4,74 @@ import java.util.ArrayList;
  * Created by Cristian on 6/5/2015.
  */
 
-public class StateBehaviour<A,B> implements Reactive<B> {
+public class StateBehaviour<A,B> {
 
     public final A key;
 
-    ArrayList<Reactive<A>> onEnterList = new ArrayList<Reactive<A>>();
-    ArrayList<Reactive<A>> onExitList = new ArrayList<Reactive<A>>();
-    ArrayList<Transition<A,B>> transitions = new ArrayList<Transition<A,B>>();
+    ArrayList<Action1<A>> onEnterList = new ArrayList<Action1<A>>();
+    ArrayList<Action1<A>> onExitList = new ArrayList<Action1<A>>();
+    ArrayList<Func1<B,A>> transitions = new ArrayList<Func1<B,A>>();
 
 
     public StateBehaviour(A key) {
         this.key = key;
     }
 
-    @Override
-    public void receive(B value) {
 
+    public A onNewData(B value) {
+
+        for (Func1<B,A> f : new ArrayList<Func1<B,A>> (transitions)) {
+            A a = f.apply(value);
+
+            if (a != key)
+                return a;
+        }
+
+        return key;
+    }
+
+    public void onEnter (A value) {
+        for (Action1<A> f : new ArrayList<Action1<A>> (onEnterList)) {
+            f.apply (value);
+        }
+    }
+
+    public void onExit (A value) {
+        for (Action1<A> f : new ArrayList<Action1<A>> (onExitList)) {
+            f.apply (value);
+        }
     }
 
     public A getKey() {
         return key;
     }
 
-    StateBehaviour<A,B> addOnEnterListener (Reactive<A> reactive) {
+    StateBehaviour<A,B> addOnEnterListener (Action1<A> reactive) {
         onEnterList.add(reactive);
         return this;
     }
 
-    StateBehaviour<A,B> removeOnEnterListener (Reactive<A> reactive) {
+    StateBehaviour<A,B> removeOnEnterListener (Action1<A> reactive) {
         onEnterList.remove(reactive);
         return this;
     }
 
-    StateBehaviour<A,B> addOnExitListener (Reactive<A> reactive) {
+    StateBehaviour<A,B> addOnExitListener (Action1<A> reactive) {
         onExitList.add(reactive);
         return this;
     }
 
-    StateBehaviour<A,B> removeOnExitListener (Reactive<A> reactive) {
+    StateBehaviour<A,B> removeOnExitListener (Action1<A> reactive) {
         onEnterList.remove(reactive);
         return this;
     }
 
-    StateBehaviour<A,B> addTransition (Transition<A,B> transition) {
+    StateBehaviour<A,B> addTransition (Func1<B,A> transition) {
         transitions.add(transition);
         return this;
     }
 
-    StateBehaviour<A,B> removeTransition (Transition<A,B> transition) {
+    StateBehaviour<A,B> removeTransition (Func1<B,A> transition) {
         transitions.remove(transition);
         return this;
     }
--- QR-ReaderExample/qRReaderExample/src/main/java/StateMachine.java
+++ QR-ReaderExample/qRReaderExample/src/main/java/StateMachine.java
@@ -1,6 +1,5 @@
-import java.util.Dictionary;
 import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
 
 /**
  * Created by Cristian on 6/5/2015.
@@ -9,6 +8,7 @@ public class StateMachine<A,B> extends StateBehaviour<A,B> implements Stream<A> 
 
     private StateBehaviour<A,B> state;
     HashMap<A,StateBehaviour<A,B>> stateMap = new HashMap<A,StateBehaviour<A,B>>();
+    LinkedList<Action1<A>> onDataListeners = new LinkedList<Action1<A>>();
 
     public StateMachine(StateBehaviour<A,B> initialState, StateBehaviour<A,B>... states) {
         super(initialState.key);
@@ -22,9 +22,42 @@ public class StateMachine<A,B> extends StateBehaviour<A,B> implements Stream<A> 
 
     void init (StateBehaviour<A,B> initialState, StateBehaviour<A,B>... states ) {
         state = initialState;
+        state.onEnter(state.key);
 
         for (StateBehaviour<A,B> stateBehaviour : states) {
             stateMap.put(stateBehaviour.key, stateBehaviour);
+        }
+    }
+
+    @Override
+    public A onNewData(B value) {
+        //Logica de StateMachine
+        handleData(value);
+        //Logica de StateBehaviour
+        return super.onNewData(value);
+    }
+
+    void handleData (B value) {
+        //Pasar datos al estado actual
+        A newKey = state.onNewData(value);
+
+        //Parar si el "key" es el mismo
+        if (newKey == state.key)
+            return;
+
+        //Obtener el nuevo estado
+        StateBehaviour<A,B> newState = stateMap.get(newKey);
+
+        //Anunciar salida y entrada en cambio de estado
+        state.onExit(newKey);
+        newState.onEnter(state.key);
+
+        //Cambiar estado
+        state = newState;
+
+        //Anunciar cambio de estado como stream de "keys"
+        for (Action1<A> f : new LinkedList<Action1<A>> (onDataListeners)) {
+            f.apply (newKey);
         }
     }
 
@@ -34,25 +67,24 @@ public class StateMachine<A,B> extends StateBehaviour<A,B> implements Stream<A> 
     }
 
     @Override
-    public void receive(B value) {
-
-    }
-
-    @Override
-    public void OnData(Reactive<A> listener) {
-
+    public void OnData(Action1<A> action) {
+        onDataListeners.add (action);
     }
 }
 
-interface Reactive<A> {
-    void receive(A value);
+interface Action1<A> {
+    void apply(A value);
 }
 
-interface Transition<A,B> {
-    A changeWith (B value);
+interface Func1<A,B> {
+    B apply(A value);
+}
+
+interface Func2<A,B,C> {
+    C apply(A v1, B v2);
 }
 
 interface Stream<A> {
 
-    void OnData (Reactive<A> listener);
+    void OnData (Action1<A> action);
 }
